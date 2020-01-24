16d082e8d189721d9df11461ef21cbd0673c9933
Anders Rune Jensen <arj03@protonmail.ch>
2020-01-24 21:22:55 +0100

    Store blobs in cache and use service worker for proper offline support

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@
   const raf = require('polyraf')
   const pull = require('pull-stream')
 
+  if (location.protocol === 'https:' && 'serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js');
+    })
+  }
+
   function httpGet(url, responseType, cb) {
     var req = new XMLHttpRequest()
     req.timeout = 2000;
@@ -29,7 +35,7 @@
       el: "#app",
       data: function() {
         return {
-          appId: "%bnTRHCyFvPIgMndnCS0+Mq4UQJEdq0cSzgktKp3jAXk=.sha256|@Oh2NQslutj+XQRJkkfzKr6gw5mt49mdYY43Rs33y3yY=.ed25519"
+          appId: "%tf+DpegXvexYmuXzB+D7AS+50UcXPjJPgZXNiw8S7jM=.sha256|@Oh2NQslutj+XQRJkkfzKr6gw5mt49mdYY43Rs33y3yY=.ed25519"
         }
       },
       methods: {
@@ -52,53 +58,60 @@
                 const blobsDir = path.join('~/.ssb/', self.appId, self.appId)
                 console.log(blobsDir)
 
-                // FIXME: maybe a better way to ensure the server can't send us bad blobs
-                pull(
-                  pull.values(Object.keys(message.content.blobs)),
-                  pull.asyncMap((appPath, cb) => {
-                    const blobId = message.content.blobs[appPath]
-                    console.log(`path: ${appPath} is blob: ${blobId}`)
-                    httpGet(SSB.net.blobs.remoteURL(blobId), 'blob', (err, data) => {
-                      if (err) return cb(err)
-                      data.arrayBuffer().then(function (buffer) {
-                        SSB.net.blobs.hash(new Uint8Array(buffer), (err, hash) => {
-                          if (err) return cb(err)
-                          if ('&' + hash != blobId) return cb(`wrong hash from server, expected ${blobId} got &${hash}`)
+                var currentCache = 'ssb-browser-demo'
+                navigator.serviceWorker.controller.postMessage(currentCache)
 
-                          if (appPath == "index.html")
-                          {
-                            const file = raf(path.join(blobsDir, appPath))
-                            file.write(0, data, (err) => {
-                              console.log("wrote blob!", file)
-                              cb(err)
+                caches.open(currentCache).then(function(cache) {
+                  pull(
+                    pull.values(Object.keys(message.content.blobs)),
+                    pull.asyncMap((appPath, cb) => {
+                      cache.match(appPath).then(function(response) {
+                        if (response != null) return cb()
+
+                        const blobId = message.content.blobs[appPath]
+                        console.log(`path: ${appPath} is blob: ${blobId}`)
+                        httpGet(SSB.net.blobs.remoteURL(blobId), 'blob', (err, data) => {
+                          if (err) return cb(err)
+                          data.arrayBuffer().then(function (buffer) {
+                            SSB.net.blobs.hash(new Uint8Array(buffer), (err, hash) => {
+                              if (err) return cb(err)
+                              if ('&' + hash != blobId) return cb(`wrong hash from server, expected ${blobId} got &${hash}`)
+
+                              if (appPath == "index.html")
+                              {
+                                cache.put(appPath, new Response(data))
+                                const file = raf(path.join(blobsDir, appPath))
+                                file.write(0, data, (err) => {
+                                  if (err) return cb(err)
+                                  console.log("wrote blob!", file)
+                                  cb()
+                                })
+                              } else
+                                cache.put(appPath, new Response(data)).then(cb)
                             })
-                          } else
-                            cb()
+                          })
+                        })
+                      })
+                    }),
+                    pull.collect((err, msgs) => {
+                      if (err) return alert(err)
+                      console.log("verified and cached all blobs")
+
+                      const indexFile = raf(path.join(blobsDir, "index.html"))
+                      indexFile.stat((err, stat) => {
+                        indexFile.read(0, stat.size, (err, data) => {
+                          if (err) throw err
+
+                          var indexHTML = data.toString()
+
+                          document.open()
+                          document.write(indexHTML)
+                          document.close()
                         })
                       })
                     })
-                  }),
-                  pull.collect((err, msgs) => {
-                    if (err) return alert(err)
-                    console.log("verified all blobs")
-
-                    const indexFile = raf(path.join(blobsDir, "index.html"))
-                    indexFile.stat((err, stat) => {
-                      indexFile.read(0, stat.size, (err, data) => {
-                        if (err) throw err
-
-                        var indexHTML = data.toString()
-
-                        for (var localPath in message.content.blobs)
-                          indexHTML = indexHTML.replace(localPath, SSB.net.blobs.remoteURL(message.content.blobs[localPath]))
-
-                        document.open()
-                        document.write(indexHTML)
-                        document.close()
-                      })
-                    })
-                  })
-                )
+                  )
+                })
               })
             })
           } else
---
+++ js/sw.js
@@ -0,0 +1,20 @@
+var currentCache = ""
+
+self.addEventListener('message', function (evt) {
+  currentCache = evt.data
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(currentCache).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) return response
+
+        // not in cache, try to fetch it from the network
+        return fetch(event.request).then(function(networkResponse) {
+          return networkResponse;
+        })
+      })
+    })
+  )
+})
