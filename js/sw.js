28eb8e3b8a1887bad29e3203cea13bb6946ac7bf
Anders Rune Jensen <anders@iola.dk>
2020-01-29 12:18:41 +0100

    Load service worker on first page load. Thanks Giarc

--- js/sw.js
+++ js/sw.js
@@ -18,3 +18,11 @@ self.addEventListener('fetch', function(event) {
     })
   )
 })
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting()) // Activate worker immediately
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim()) // Become available to all pages
+})
